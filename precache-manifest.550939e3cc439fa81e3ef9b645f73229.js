8f104edfffe140281d33da57502461ae1a64bce7
grzechz <grzegorz.adam.zadora@gmail.com>
2020-08-10 22:03:35 +0200

    Deploy

---
+++ precache-manifest.550939e3cc439fa81e3ef9b645f73229.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cefbb3db24f743b64af1f97f51cc055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb85ef7e2a70513ab125",
+    "url": "/static/js/main.de5f3e21.chunk.js"
+  },
+  {
+    "revision": "77e885662a0947329ab5",
+    "url": "/static/js/runtime-main.03ae3025.js"
+  }
+]);
